1ca50c030534f007967b88dd28f6e563c1534934
106360109 <t106360109@ntut.org.tw>
2020-07-25 15:16:33 +0800

    fix '-'apper at front

--- app/src/main/java/dev/edmt/androidcamerarecognitiontext/MainActivity.java
+++ app/src/main/java/dev/edmt/androidcamerarecognitiontext/MainActivity.java
@@ -169,8 +169,8 @@ public class MainActivity extends AppCompatActivity {
                                     }
                                     for (int i = 2; i < t.length(); i++)
                                         tmp += t.charAt(i);
-                                    t = tmp;
-                                    tmp = c.check(tmp);
+                                    t = tmp.replace("-","");
+                                    tmp = c.check(tmp.replace("-",""));
                                     if (dateFound) {
                                         if (flag) {
                                             textView2.setTextSize(36);
